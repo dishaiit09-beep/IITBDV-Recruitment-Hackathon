167f553962f59b0a9176daa174a804883462b7ee
disha <dishaiit09@gmail.com>
2026-03-16 20:59:01 +0530

    added code for planner.py

--- PPC/racing_hkt_windows/dist/participant/planner.py
+++ PPC/racing_hkt_windows/dist/participant/planner.py
@@ -29,6 +29,7 @@ def plan(cones: list[dict]) -> list[dict]:
          You can also compute a curvature-optimised racing line.
     """
     path = []
+
     # TODO: implement your path planning here
     blue = np.array([[cone["x"], cone["y"]] for cone in cones if cone["side"] == "left"])
     yellow = np.array([[cone["x"], cone["y"]] for cone in cones if cone["side"] == "right"])
@@ -36,7 +37,36 @@ def plan(cones: list[dict]) -> list[dict]:
     # implement a planning algorithm to generate a path from the blue and yellow cones
 
 
+ n = min(len(blue), len(yellow))
 
+    midpoints = []
+
+    for i in range(n):
+
+        lx = blue[i][0]
+        ly = blue[i][1]
+
+        rx = yellow[i][0]
+        ry = yellow[i][1]
+
+        mx = (lx + rx) / 2
+        my = (ly + ry) / 2
+
+        midpoints.append((mx, my))
+
+    for i in range(len(midpoints)):
+     if i == 0 or i == len(midpoints) - 1:
+            x, y = midpoints[i]
+
+        else:
+            x1, y1 = midpoints[i-1]
+            x2, y2 = midpoints[i]
+            x3, y3 = midpoints[i+1]
+
+            x = (x1 + x2 + x3) / 3
+            y = (y1 + y2 + y3) / 3
+
+        path.append({"x": float(x), "y": float(y)})
 
 
 
